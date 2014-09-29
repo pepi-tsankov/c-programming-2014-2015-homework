4542e16ab0d807d1b1b566212d316da59c3f8333
valentin <valentinstoqnov98@gmail.com>
2014-09-29 21:59:39 +0300

    Add Hello world program

---
+++ B/06_Valentin_Stoyanov/Homework_2/homework3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+int random=303;
+printf("number=%d",random);
+printf("\n");
+return 0;
+}
---
+++ B/06_Valentin_Stoyanov/Homework_2/homework5.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main () {
+char g=71,a='a',t='T',e=101;
+printf("%c%c%c%c\n",g,a,t,e);
+return 0;
+}
