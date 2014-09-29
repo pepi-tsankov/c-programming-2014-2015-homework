d7ac547b97deb9176c8086eaee39a318207c39fa
brondos <brondos63@yahoo.com>
2014-09-30 02:36:27 +0300

    Update UsedToBeHello.c

--- B/26_Russi_Russev/Homework-2/UsedToBeHello.c
+++ B/26_Russi_Russev/Homework-2/UsedToBeHello.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int main()
-	{
-		int numn1 = 1;
-		int numn2 = 10;
-		int numn3 = 11;
-		int numn4 = 100;
-			printf("NumberN1 = %d\n" "NumberN2 = %d\n" "NumberN3 = %d\n" "NumberN4 = %d\n", numn1, numn2, numn3, numn4);
-		return 0;
-	}
+
+	int main()
+		{
+			float Plava6taZapetaq = 255.99999;
+				printf("FloatingNumber = %3.5f\n", Plava6taZapetaq);
+	return 0;
+		}
