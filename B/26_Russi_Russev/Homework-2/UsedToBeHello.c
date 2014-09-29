a63aebd2f43a337e187532ae7c0ce6895c802f01
brondos <brondos63@yahoo.com>
2014-09-30 02:25:48 +0300

    Update UsedToBeHello.c

--- B/26_Russi_Russev/Homework-2/UsedToBeHello.c
+++ B/26_Russi_Russev/Homework-2/UsedToBeHello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-	main()
-		{
-			printf("Hello World!\n");
-return 0;
-
-		}
+int main()
+	{
+		int numn1 = 1;
+		int numn2 = 10;
+		int numn3 = 11;
+		int numn4 = 100;
+			printf("NumberN1 = %d\n" "NumberN2 = %d\n" "NumberN3 = %d\n" "NumberN4 = %d\n", numn1, numn2, numn3, numn4);
+		return 0;
+	}
