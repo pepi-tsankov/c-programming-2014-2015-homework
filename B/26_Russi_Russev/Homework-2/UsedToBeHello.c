8c31af8f07f40542d59bad7d1b11a03d8efe6744
brondos <brondos63@yahoo.com>
2014-09-30 02:36:49 +0300

    Update UsedToBeHello.c

--- B/26_Russi_Russev/Homework-2/UsedToBeHello.c
+++ B/26_Russi_Russev/Homework-2/UsedToBeHello.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
 	int main()
 		{
-			float Plava6taZapetaq = 255.99999;
-				printf("FloatingNumber = %3.5f\n", Plava6taZapetaq);
+			char char1 = 'H';
+			char char2 = 'e';
+			char char3 = 'l';
+			char char4 = 'l';
+			char char5 = 'o';
+			char char6 = 'W';
+			char char7 = 'o';
+			char char8 = 'r';
+			char char9 = 'l';
+			char char10 = 'd';
+			char char11 = '!';
+				printf("%c%c%c%c%c%c%c%c%c%c%c \n", char1, char2, char3, char4, char5, char6, char7, char8, char9, char10, char11);
 	return 0;
 		}
