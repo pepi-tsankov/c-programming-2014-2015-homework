347b7216345ffab9af3880a70a080af808f75801
vicky98 <vikiga98@gmail.com>
2014-09-30 08:09:28 +0000

    Added three chars

--- A/07_Viktoria_Admasu/Homework_2/printf_examples.c
+++ A/07_Viktoria_Admasu/Homework_2/printf_examples.c
@@ -4,6 +4,16 @@ int main()
 int num;
 num = 736;
 
+float flnum = 2.5;
+
+char symbol1 = 70;
+char symbol2 = 75;
+char symbol3 = 65;
+
 printf ("Number: %d\n", num);
+printf ("Floating point number: %f\n", flnum);
+printf ("Symbol 1: %c\n", symbol1);
+printf ("Symbol 2: %c\n", symbol2);
+printf ("Symbol 3: %c\n", symbol3);
 return 0;
 }
