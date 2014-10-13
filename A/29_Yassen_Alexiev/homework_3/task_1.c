f05d184160359ade794a56960fac313a0bb2f11b
Streetfighter4 <yassen4o222@abv.bg>
2014-10-13 22:38:40 +0300

    write program1

---
+++ A/29_Yassen_Alexiev/homework_3/task_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int а;
+    for (a=97;a<=122;a++) {
+        printf("%c - %d\n",a,a);
+    }
+    for (a=65;a<=90;a++) {
+        printf("%c - %d\n",a,a);
+    }
+    for(а=48;а<=57;а++) {
+        printf ("%c - %d\n",a,a);
+    }
+    return 0;
+}
