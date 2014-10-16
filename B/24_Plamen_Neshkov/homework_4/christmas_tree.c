5c89bf0e3378af76fe4400ee2c50dd91b43009d3
Plamen Neshkov <plamen.neshkov@outlook.com>
2014-10-16 23:20:52 +0300

    Added christmas_tree.c

---
+++ B/24_Plamen_Neshkov/homework_4/christmas_tree.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+	char asterisk = '*';
+	int i, ii, height, start = 1, end = 1;
+
+	height = 4;
+	for (i = 1; i <= height; i++) 
+	{
+		for (ii = 1; ii <= height-i; ii++)
+		{
+			printf(" ");
+		}
+		for (start = 1; start <= end; start++) 
+		{
+			printf("%c", asterisk);
+		}
+		end += 2;
+		printf("\n");
+	}
+	
+	end = 1;
+	height = 7;
+	for (i = 1; i <= height; i++) 
+	{
+		for (ii = 1; ii <= height-i; ii++)
+		{
+			printf(" ");
+		}
+		for (start = 1; start <= end; start++) 
+		{
+			printf("%c", asterisk);
+		}
+		end += 2;
+		printf("\n");
+	}
+
+	end = 1;
+	height = 11;
+	for (i = 1; i <= height; i++) 
+	{
+		for (ii = 1; ii <= height-i; ii++)
+		{
+			printf(" ");
+		}
+		for (start = 1; start <= end; start++) 
+		{
+			printf("%c", asterisk);
+		}
+		end += 2;
+		printf("\n");
+	}
+	return 0;
+}
