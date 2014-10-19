8a2f11f35405725189bb4973f7859f665e8dd7d1
paresseux <apopov98@gmail.com>
2014-10-19 15:15:40 +0300

    406567

--- B/01_Asen_Popov/Homework_4/reverse_number.c
+++ B/01_Asen_Popov/Homework_4/reverse_number.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	int n=987697, obr=0;
-	int n1=n;
+	long int n=406567, obr=0;
+	long int n1=n;
 	while (n!=0) {
 		obr=obr*10;
 		obr=obr+n%10;
 		n=n/10;
 	}
-	printf("%d => %d\n", n1, obr);
+	printf("%ld => %ld\n", n1, obr);
 	return 0;
 }
