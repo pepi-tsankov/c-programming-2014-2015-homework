b6a75612c6d847573d2ab5cc92b007993de60c0b
Luchezar <luchobg@abv.bg>
2014-10-19 19:17:58 +0300

    made the reverse demostrate with the given numbers

--- B/16_Luchezar_Ivanov/homework_4/reverse_number.c
+++ B/16_Luchezar_Ivanov/homework_4/reverse_number.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-int main()
+int reversenum(long long num)
 {
-long long num,a,reverse,add;
+long long a,reverse,add;
 int mod,i,j;
-scanf("%lld",&num);
 a=num;
 i=0;
 while(a>0)
@@ -25,6 +24,15 @@ while(a>0)
 	reverse=reverse+add;
 	i=i-1;
 	}
-printf("%lld\n",reverse);
+
+return reverse;
+}
+int main()
+{
+printf("5170984 %d\n", reversenum(5170984));
+printf("406567 %d\n", reversenum(406567));
+printf("368750 %d\n", reversenum(368750));
+printf("2369510 %d\n", reversenum(2369510));
+printf("2183293 %d\n", reversenum(2183293));
 return 0;
 }
