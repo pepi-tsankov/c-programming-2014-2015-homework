30b07f83a87bd6bc414787535a7e6e43c882251d
Antonio Mindov <amindov@abv.bg>
2014-10-19 12:24:43 +0300

    Update reverse_number.c

--- A/03_Antonio_Mindov/homework_4/reverse_number.c
+++ A/03_Antonio_Mindov/homework_4/reverse_number.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int main ()
+int ReverseNumber(int numb)
 {
-	int numb = 2183293;
 	int reverse=0;
 	
 	while(numb>0)
@@ -12,8 +11,17 @@ int main ()
 		reverse += remainder;
 		reverse *= 10;
 	}
-	reverse /= 10;
 	
-	printf("%d\n",reverse);
+	reverse /= 10;
+	return reverse;
+}
+
+int main ()
+{
+	printf("%d\n",ReverseNumber(5170984));
+	printf("%d\n",ReverseNumber(406567));
+	printf("%d\n",ReverseNumber(368750));
+	printf("%d\n",ReverseNumber(2369510));
+	printf("%d\n",ReverseNumber(2183293));
 	return 0;
 }
