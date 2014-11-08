748e7001c87c77a0b1afb23723b33aa33fd748af
4o4oTo <4o4olino70@gmail.com>
2014-11-08 12:02:03 -0800

    Histogram

---
+++ A/14_Ivan-Asen_Ovedenski/Homework_5/histogram.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ 	
+int main () {
+	int range;
+	printf("\nInsert range : ");
+	scanf("%d",&range);
+	if (range < 0) {
+		scanf("%d",&range);
+	}
+	int length;
+	printf("Insert length : ");
+	scanf("%d",&length);
+	if (length < 0){
+		scanf("%d",&length);
+	}
+	printf("Insert numbers : ");
+	int i,arr[length] ;
+	for (i = 0;i < length;i++) {
+		scanf("%d",&arr[i]);
+		if (arr[i] > range){
+			scanf("%d",&arr[i]);
+		}
+	}
+	for (i = 0;i < range;i++){
+		int b;
+		printf("\n%d: ",i);
+		for (b = 0;b < length;b++){	
+			if (arr[b] == i){
+				printf("*");
+			}
+		}
+	}
+	printf("\nN: %d\n ",range);
+	return 0;	
+}
