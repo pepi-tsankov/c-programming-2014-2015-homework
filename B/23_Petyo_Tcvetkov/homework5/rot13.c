9d1712280c1524ef420b976da93d9e0f780102f1
petiocvetkov <petiopetio@mail.bg>
2014-11-10 23:55:52 +0200

    @homework@

---
+++ B/23_Petyo_Tcvetkov/homework5/rot13.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+void rod13(char b[200]){
+char arrr[200];
+int i;
+for( i=0;b[i]!='\0';i++){
+	if (b[i]>90){
+		if ((arrr[i]=b[i]+13)>122){
+		arrr[i]=b[i]+13-26;
+		}
+		else
+		arrr[i]=b[i]+13;
+
+		}
+	};
+	
+		if((arrr[i]=b[i]+13)<90){
+		arrr[i]=b[i]+13-26;
+		}
+		else
+		{
+		arrr[i]=b[i]+13;
+		};
+		arrr[i]='\0';
+	printf("text:%s\n",arrr);
+	
+}
+
+
+
+
+int main(){
+
+char d,t[200];
+while(d!='q'){
+scanf("%c",&d);
+if(d=='c'){
+scanf("%s",t);
+rod13(t);
+}
+else
+if(d=='d'){
+scanf("%s",t);
+rod13(t);
+}
+}
+}
