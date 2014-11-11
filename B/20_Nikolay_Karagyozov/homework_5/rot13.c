bfdadeef6913b8c7cde550453311070240d08d67
Nicky <newmast@abv.bg>
2014-11-11 10:23:53 +0200

    hw 5 done

---
+++ B/20_Nikolay_Karagyozov/homework_5/rot13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void rot13(char masiv2[1000], int first){
+	if(first == 0){
+		int i;
+		for (i = 0; masiv2[i] != '\0' && masiv2[0] !='c' && masiv2[0] !='d'; i++){
+			if (masiv2[i] >= 'A' && masiv2[i] <= 'Z'){
+				int temp = masiv2[i];
+				int num = temp - 'A';
+				num = (num + 13) % 26;
+				masiv2[i] = 'A' + num;
+			} 
+			else 
+			if (masiv2[i] >= 'a' && masiv2[i] <= 'z'){
+				int temp = masiv2[i];
+				int num = temp - 'a';
+				num = (num + 13) % 26;
+				masiv2[i] = 'a' + num;
+			}
+		}
+		printf("%s ", masiv2);
+	}
+}
+int main() {
+	char masiv[1000];
+	int first = 1;
+
+	while(scanf("%1000s", masiv) && (masiv[0] != 'q')){
+		rot13(masiv, first);
+		first = 0;
+	}
+	return 0;
+}
