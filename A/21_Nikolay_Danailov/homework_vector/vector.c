841814a8954698cd8a0643b69baff750ea7486ab
Nikolay Danailov <frostblooded@yahoo.com>
2015-03-12 08:37:58 +0200

    Made the vector homework

---
+++ A/21_Nikolay_Danailov/homework_vector/vector.c
@@ -0,0 +1,132 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+// index, size, data
+
+// [0, 0, 0, 0, 0]
+// [0, -1, -2, -3, -4 ...]
+
+#define STARTING_SIZE 5
+
+struct vector_t {
+	int *data;
+	int index, size;
+};
+
+void vector_init(struct vector_t*);
+void vector_destroy(struct vector_t*);
+int vector_get_size(struct vector_t);
+int vector_resize(struct vector_t*);
+void transfer_data(int*, int, int*);
+int vector_at(struct vector_t, int index);
+void vector_push_back(struct vector_t*, int value);
+int vector_pop_back(struct vector_t*);
+
+int main() 
+{
+	struct vector_t v;
+	vector_init(&v);
+
+	printf("\n");
+	printf("Pushing into the vector:\n");
+
+	int i;
+	for (i = 0; i < 20; ++i)
+	{
+		vector_push_back(&v, i + 1);
+
+		if(i % 5 == 0)
+		{
+			printf("vector_index: %d, vector_size: %d\n", v.index, v.size); //to demonstrate that the size doubles
+		}
+	}
+
+	printf("\n");
+	printf("Poping out of the vector:\n");
+
+	for (i = 0; i < 20; ++i)
+	{
+		printf("Pop: %d\n", vector_pop_back(&v));
+
+		if(i % 5 == 0)
+		{
+			printf("vector_index: %d, vector_size: %d\n", v.index, v.size); // to demonstrate that the size reduces
+		}
+	}
+
+	return 0;
+}
+
+void vector_init(struct vector_t* v) {
+	v->size = STARTING_SIZE;
+	v->index = 0;
+	v->data = (int*) malloc(v->size * sizeof(int));
+}
+
+void vector_destroy(struct vector_t* v){
+	free(v->data);
+	v->size = 0;
+	v->index = 0;
+}
+
+int vector_get_size(struct vector_t v) {
+	return v.index;
+}
+
+void vector_push_back(struct vector_t* v, int value) {
+	if (v->index == v->size)
+		vector_resize(v);
+
+	v->data[v->index++] = value;
+}
+
+int vector_at(struct vector_t v, int index) {
+	if(index < 0 || index >= v.size)
+		return INT_MIN; //for error returns the minimum value of int, because it is the least possible number to actually be in the vector
+
+	return v.data[index];
+}
+
+int vector_pop_back(struct vector_t* v) {
+	if(v->index == 0)
+		return INT_MIN; //for error returns the minimum value of int, because it is the least possible number to actually be in the vector
+
+	if(v->index <= v->size / 2 && v->size > STARTING_SIZE)
+		vector_resize(v);
+
+	return v->data[--v->index];
+}
+
+void transfer_data(int *old_data, int old_data_size, int *new_data)
+{
+	int i;
+	for(i = 0; i < old_data_size; ++i)
+	{
+		new_data[i] = old_data[i];
+	}
+}
+
+int vector_resize(struct vector_t *v)		//returns: 0 if it worked out OK
+{											//-1 if something went wrong
+	if (v->index == v->size)				
+	{
+		v->size *= 2;
+	}
+	else if(v->index <= v->size / 2)
+	{
+		v->size /= 2;
+	}
+
+	int *new_data = (int*)malloc(sizeof(int) * v->size);
+
+	transfer_data(v->data, v->size, new_data);
+
+	free(v->data);
+	v->data = new_data;
+
+	if(v->data == NULL)
+		return -1;
+
+	return 0;
+}
