3b01eb609cc9d691e07cc733ed17c1cee66ff273
Antonio <amindov@abv.bg>
2015-03-15 21:25:41 +0200

    Made vector homework

---
+++ A/03_Antonio_Mindov/homework_vector/main.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+// index, size, data
+// [0, 0, 0, 0, 0]
+// [0, -1, -2, -3, -4 ...]
+
+struct vector_t {
+    int *data;
+    int index, size;
+};
+
+void vector_init(struct vector_t*);
+void vector_destroy(struct vector_t*);
+int vector_get_size(struct vector_t);
+int vector_resize(struct vector_t* v,int side);
+int vector_at(struct vector_t, int index);
+void vector_push_back(struct vector_t*, int value);
+int vector_pop_back(struct vector_t*);
+
+int main() {
+    struct vector_t myVector;
+    vector_init(&myVector);
+    vector_push_back(&myVector,3);
+    vector_push_back(&myVector,1);
+    vector_push_back(&myVector,4);
+    vector_push_back(&myVector,1);
+    vector_push_back(&myVector,5);
+    vector_push_back(&myVector,9);
+    vector_push_back(&myVector,2);
+    vector_push_back(&myVector,6);
+    printf("Size: %d\n", vector_get_size(myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Size: %d\n", vector_get_size(myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Pop: %d\n", vector_pop_back(&myVector));
+    printf("Size: %d\n", vector_get_size(myVector));
+    vector_destroy(&myVector);
+    return 0;
+}
+
+void vector_init(struct vector_t* v) {
+    v->size = 5;
+    v->index = 0;
+    v->data = (int*) malloc(v->size * sizeof(int));
+}
+
+void vector_destroy(struct vector_t* v){
+    free(v->data);
+    v->size = 0;
+    v->index = 0;
+}
+
+int vector_get_size(struct vector_t v) {
+    return v.index;
+}
+
+void vector_push_back(struct vector_t* v, int value) {
+    if(v->index >= v->size)
+    {
+        vector_resize(v, 0);
+    }
+    v->data[v->index++] = value;
+}
+
+int vector_at(struct vector_t v, int index) {
+    if(index < 0 || index >= v.index)
+    {
+        return INT_MAX;
+    }
+
+    return v.data[index];
+}
+
+int vector_pop_back(struct vector_t* v) {
+    if(v->index <= 0)
+    {
+        return INT_MAX;
+    }
+    if(v->index <= v->size / 2)
+    {
+        vector_resize(v, 1);
+    }
+
+    return v->data[--v->index];
+}
+
+int vector_resize(struct vector_t* v, int makeLower)
+{
+    int i;
+    int oldData[v->size];
+
+    for(i = 0; i < v->index; i++)
+    {
+        oldData[i] = vector_at(*v,i);
+    }
+
+    if(makeLower==0)
+    {
+        v->size *= 2;
+    }
+    else
+    {
+        v->size /= 2;
+    }
+
+    free(v->data);
+    v->data = (int*) malloc(v->size * sizeof(int));
+
+    for(i = 0; i < v->index; i++)
+    {
+        v->data[i] = oldData[i];
+    }
+    
+    return v->size;
+}
